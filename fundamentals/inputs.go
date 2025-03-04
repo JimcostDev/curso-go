e311b50daaffe84024ec75f26de4a706be24182d
JimcostDev <rjak727@gmail.com>
2025-03-04 18:51:11 +0100

    add ejemplo inputs

--- fundamentals/inputs.go
+++ fundamentals/inputs.go
@@ -9,6 +9,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func Inputs() {
@@ -39,4 +40,27 @@ func Inputs() {
 	//frase = strings.TrimSpace(frase)    // Limpiar espacios al inicio y final
 	fmt.Printf("[%s]\n", frase)
 	fmt.Printf("Longitud: %d\n", len(frase))
+
+	// 5. Ejemplo
+	example()
+}
+
+func example() {
+	println("\nOtro ejemplo:")
+	// captura nombre y edad
+	var nombre string
+	var edad int
+	fmt.Print("Ingresa tu nombre y edad: ")
+	fmt.Scan(&nombre, &edad)
+	fmt.Printf("Nombre: %s, Edad: %d\n", nombre, edad)
+
+	// Limpiar el buffer de entrada
+	bufio.NewReader(os.Stdin).ReadString('\n')
+
+	// captura frase
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Escribe tu frase favorita: ")
+	scanner.Scan()
+	frase := strings.TrimSpace(scanner.Text()) // elimina espacios en blanco
+	fmt.Println("Tu frase es:", frase)
 }
